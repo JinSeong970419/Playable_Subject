e2df5dd9c8b6a0887a262d1ad92b425d95fb5e76
JinSeong <wlstjd2550@gmail.com>
2025-07-09 09:47:11 +0900

    게임 종료 이벤트 마무리

--- Assets/@Scripts/GameController/GameController.cs
+++ Assets/@Scripts/GameController/GameController.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum GameState
 {
@@ -13,12 +14,24 @@ public class GameController : MonoBehaviour
     [Header("스포너 오브젝트")]
     public RailSpawner railSpawner;
     public TraySpawner traySpawner;
+    public GameObject endGamePanel;
+    public Button playNowButton;
 
     private GameState _state = GameState.Ready;
 
     // 게임 시간
     [LunaPlaygroundField("Game End Delay (sec)", 20, "Delay in seconds before game ends after action")]
     public int endTime = 20;
+
+    private void Awake()
+    {
+        endGamePanel.SetActive(false);
+        playNowButton.onClick.AddListener(() =>
+        {
+            Luna.Unity.Playable.InstallFullGame();
+            Luna.Unity.LifeCycle.GameEnded();
+        });
+    }
 
     void Start()
     {
@@ -54,9 +67,7 @@ public class GameController : MonoBehaviour
             traySpawner.StopSpawning();
 
         // TODO 게임 결과 표시
-        Luna.Unity.Playable.InstallFullGame();
-
-        Luna.Unity.LifeCycle.GameEnded();
+        endGamePanel.SetActive(true);
     }
 
     public GameState GetState()
